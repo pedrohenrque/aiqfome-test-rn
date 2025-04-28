d3102ea48f45203064874613838800551756f763
Pedro Hennrique <pedrosilveira07@outlook.com>
2025-04-27 22:33:27 -0300

    feat(product): added button notification

--- modules/products/product.view.tsx
+++ modules/products/product.view.tsx
@@ -1,13 +1,15 @@
 import { FavoriteToggleButton } from '@/components'
+import { useNotification } from '@/hooks/Notifications'
 import { ProductProps } from '@/services/Products/product.type'
 import React from 'react'
-import { FlatList, View } from 'react-native'
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
 import { ProductCard } from './components'
 import { styles } from './product.styles'
 import { IProduct } from './product.type'
 
 export function ProductView(props: IProduct) {
   const { handleShowFavorites, onToggle, products, toggleFavorite } = props
+  const { sendLocalNotification } = useNotification()
 
   return (
     <View style={styles.container}>
@@ -15,6 +17,16 @@ export function ProductView(props: IProduct) {
         onToggle={handleShowFavorites}
         showOnlyFavorites={onToggle}
       />
+      <TouchableOpacity
+        onPress={() =>
+          sendLocalNotification(
+            'Teste de Notificação',
+            'Descrição de notificação de teste'
+          )
+        }
+      >
+        <Text>Teste Notification</Text>
+      </TouchableOpacity>
       <FlatList
         data={products}
         keyExtractor={(item) => item.id}
